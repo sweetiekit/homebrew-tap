3ec641f20fc49fa6a1deeada795efbc2219280cf
Shawn Presser <shawnpresser@gmail.com>
2019-05-20 23:41:51 -0500

    Fix camelcase

--- node-ios-simulator.rb
+++ node-ios-simulator.rb
@@ -1,4 +1,4 @@
-class NodeIOSSimulator < Formula
+class NodeIosSimulator < Formula
   desc "Node.JS 12 built for iOS Simulator"
   homepage "https://github.com/shawwn/node/tree/ios-simulator-release"
   head "https://github.com/shawwn/node.git", :branch => "ios-simulator-release"
--- node-ios.rb
+++ node-ios.rb
@@ -1,4 +1,4 @@
-class NodeIOS < Formula
+class NodeIos < Formula
   desc "Node.JS 12 built for iOS"
   homepage "https://github.com/shawwn/node/tree/ios-release"
   head "https://github.com/shawwn/node.git", :branch => "ios-release"
